6e91ba103bf0e99277527a33f8bad83ef2b4c618
RinehartZ <90218663+RinehartZ@users.noreply.github.com>
2025-09-25 22:06:24 +0800

    Update QB504Install.sh

--- Tools/QB504Install.sh
+++ Tools/QB504Install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <user> <password> [<port> <qb_up_port>] [<bbr_option>]"
+    echo "Usage: $0 <user> <password> [<port> <qb_up_port>] [<bbr_option>] [<qb_version>]"
     echo "bbr_option: bbrx / bbry / bbrz / none (默认 none)"
+    echo "qb_version: 504 / 505 / 512 (默认 504)"
     exit 1
 fi
 
@@ -11,6 +12,7 @@ PASSWORD=$2
 PORT=${3:-8080}
 UP_PORT=${4:-23333}
 BBR_OPTION=${5:-none}
+QB_VERSION=${6:-504}  # 新增，默认 504
 
 RAM=$(free -m | awk '/^Mem:/{print $2}')
 CACHE_SIZE=$((RAM / 8))
@@ -23,6 +25,7 @@ apt install -y curl htop vnstat
 
 systemctl stop qbittorrent-nox@$USER
 
+# BBR 部分保持不变
 case "$BBR_OPTION" in
     bbrx)
         echo "启用 BBRx..."
@@ -53,12 +56,28 @@ esac
 
 systemARCH=$(uname -m)
 if [[ $systemARCH == x86_64 ]]; then
-    wget -O /usr/bin/qbittorrent-nox "https://raw.githubusercontent.com/RinehartZ/Seedbox-Components/refs/heads/main/Torrent%20Clients/qBittorrent/x86_64/qBittorrent-5.0.4%20-%20libtorrent-v1.2.20/qbittorrent-nox"
+    case "$QB_VERSION" in
+        504)
+            QB_URL="https://raw.githubusercontent.com/RinehartZ/Seedbox-Components/refs/heads/main/Torrent%20Clients/qBittorrent/x86_64/qBittorrent-5.0.4%20-%20libtorrent-v1.2.20/qbittorrent-nox"
+            ;;
+        505)
+            QB_URL="https://raw.githubusercontent.com/RinehartZ/Seedbox-Components/refs/heads/main/Torrent%20Clients/qBittorrent/x86_64/qBittorrent-5.0.5%20-%20libtorrent-v1.2.20/qbittorrent-nox"
+            ;;
+        512)
+            QB_URL="https://raw.githubusercontent.com/RinehartZ/Seedbox-Components/refs/heads/main/Torrent%20Clients/qBittorrent/x86_64/qBittorrent-5.1.2%20-%20libtorrent-v1.2.20/qbittorrent-nox"
+            ;;
+        *)
+            echo "无效的 qb_version: $QB_VERSION"
+            exit 1
+            ;;
+    esac
+    wget -O /usr/bin/qbittorrent-nox "$QB_URL"
 elif [[ $systemARCH == aarch64 ]]; then
     wget -O /usr/bin/qbittorrent-nox "https://raw.githubusercontent.com/RinehartZ/Seedbox-Components/refs/heads/main/Torrent%20Clients/qBittorrent/ARM64/qBittorrent-5.0.4%20-%20libtorrent-v1.2.20/qbittorrent-nox"
 fi
 chmod +x /usr/bin/qbittorrent-nox
 
+# 配置文件及其他保持不变
 CONFIG_FILE="/home/$USER/.config/qBittorrent/qBittorrent.conf"
 sed -i "s/WebUI\\\\Port=[0-9]*/WebUI\\\\Port=$PORT/" $CONFIG_FILE
 sed -i "s/Connection\\\\PortRangeMin=[0-9]*/Connection\\\\PortRangeMin=$UP_PORT/" $CONFIG_FILE
