def5af86308d68276dbbac9737a2247c92c42fa4
RinehartZ <90218663+RinehartZ@users.noreply.github.com>
2026-09-01 20:30:27 +0800

    Update more_qb.sh

--- Tools/more_qb.sh
+++ Tools/more_qb.sh
@@ -27,7 +27,7 @@ START_WEB_PORT=${START_WEB_PORT:-$DEFAULT_WEB_PORT}
 START_BT_PORT=${START_BT_PORT:-$DEFAULT_BT_PORT}
 
 # 源文件和目标文件路径
-SOURCE_FILE="/etc/systemd/system/seedbox-qbittorrent-*.service"
+SOURCE_FILE="/etc/systemd/system/seedbox-qbittorrent-.service"
 
 # 检查源文件是否存在
 if [ ! -f "$SOURCE_FILE" ]; then
